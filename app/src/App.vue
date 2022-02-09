8cbe60d63a26977254a03b5c0d61e0df9b7d9fc9
ishidad2 <d.m.y.s.happy.life@gmail.com>
2022-02-10 01:11:20 +0900

    update

--- app/src/App.vue
+++ app/src/App.vue
@@ -57,11 +57,11 @@
 </template>
 
 <script>
-const NODE = "https://yamatanoorochi.sfn.tools:3001";      //testnet
-const raw_address = "TD4B6MK7BKOKA3WJH74YDZODG5QN5DFXBCOSWNQ";//testnet
+// const NODE = "https://yamatanoorochi.sfn.tools:3001";      //testnet
+// const raw_address = "TD4B6MK7BKOKA3WJH74YDZODG5QN5DFXBCOSWNQ";//testnet
 
-// const NODE = "https://dual-1.nodes-xym.work:3001";
-// const raw_address = "NB3YJ6FZ7CRZLMILAW4S6Y3ONUP5TG5GZXGFWNA";
+const NODE = "https://dual-1.nodes-xym.work:3001";
+const raw_address = "NB3YJ6FZ7CRZLMILAW4S6Y3ONUP5TG5GZXGFWNA";
 
 import ListView from './components/List.vue';
 import NewTransaction from './components/NewTransactionCard.vue';
