5df3690ff631e1dc0c93aba5f666cce01b88eac3
ishidad2 <d.m.y.s.happy.life@gmail.com>
2022-02-28 14:13:02 +0900

    メタデータ設定フォーム実装

---
+++ app/src/views/Home.vue
@@ -0,0 +1,237 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" lg="6" ms="12">
+          <v-card elevation="2" class="mb-3">
+            <v-card-title>
+              Garden Address
+            </v-card-title>
+            <v-card-text>
+              <h3>{{ g_rawAddress.plain() }}<v-icon class="ml-3" @click="copy">mdi-content-copy</v-icon></h3>
+            </v-card-text>
+            <v-card-text>
+              Please send your mosaic to the address above. (Except XYM)<br />
+              Money transfer from multisig addresses is not supported. Aggregate transactions are not supported.
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" lg="6" ms="12">
+          <v-card elevation="2" class="mb-3">
+            <v-card-title>
+              User Address
+            </v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" lg="9" ms="12">
+                  <v-text-field v-model="rawAddress" disabled label="今後実装予定"></v-text-field>
+                </v-col>
+                <v-col>
+                  <v-btn class="btn" disabled>SSSへ署名を送る</v-btn>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <new-transaction ref="tx" :histories="histories"></new-transaction>
+      <list-view ref="mosaic_list" :p-items="mosaics"></list-view>
+      <v-snackbar 
+        absolute
+        bottom
+        :color="color"
+        outlined
+        v-model="snackbar">
+          {{ snackbarMsg }}
+      </v-snackbar>
+    </v-container>
+  </div>
+</template>
+
+<script>
+// const NODE = "https://yamatanoorochi.sfn.tools:3001";      //testnet
+// const raw_address = "TD4B6MK7BKOKA3WJH74YDZODG5QN5DFXBCOSWNQ";//testnet
+
+const NODE = "https://dual-1.nodes-xym.work:3001";
+const raw_address = "NB3YJ6FZ7CRZLMILAW4S6Y3ONUP5TG5GZXGFWNA";
+
+import ListView from '../components/List.vue';
+import NewTransaction from '../components/NewTransactionCard.vue';
+
+import * as symbolSdk from 'symbol-sdk';
+import * as op from 'rxjs/operators';
+import {ng_mosaic_lists} from '../../config/nglist';
+
+export default {
+  components: {
+    ListView,
+    NewTransaction
+  },
+  data: function (){
+    return {
+      mosaics: [],
+      g_rawAddress: symbolSdk.Address.createFromRawAddress(raw_address),
+      rawAddress: '',
+      nglist: ng_mosaic_lists,
+      accountRepository: null,
+      namespaceRepository: null,
+      mosaicRepository: null,
+      transactionRepository: null,
+      snackbar: false,
+      multiLine: true,
+      listener: null,
+      init: true,
+      histories: {
+        receive_mosaics: [],
+        send_mosaics: []
+      },
+      color: '',
+      snackbarMsg: '',
+      recipientAudio: new Audio(require('@/assets/audio/ding.ogg')),
+    }
+  },
+  created(){
+    this.$nextTick(()=>{
+      const repositoryFactory = new symbolSdk.RepositoryFactoryHttp(NODE);
+      this.accountRepository = repositoryFactory.createAccountRepository();
+      this.transactionRepository = repositoryFactory.createTransactionRepository()
+      this.mosaicRepository = repositoryFactory.createMosaicRepository();
+      this.namespaceRepository = repositoryFactory.createNamespaceRepository();
+
+      this.getAccountInfo();
+      this.getTransfers();
+      this.openListener();
+    });
+  },
+  destroyed() {
+    this.closeListenr();
+  },
+  methods: {
+    async getTransfers(){
+      const txs = await this.transactionRepository.search({
+        address: this.g_rawAddress,
+        group: symbolSdk.TransactionGroup.Confirmed,
+        pageSize: 10,
+        order:"desc"}).toPromise();
+      this.parseTx(txs.data);
+      this.init = false;
+    },
+    getAccountInfo(){
+      this.accountRepository.getAccountInfo(this.g_rawAddress)
+      .pipe(
+        op.mergeMap(_=>_.mosaics),
+        op.filter(mo=>{
+          return !this.nglist.find((mos) => mos === mo.id.toHex())
+        })
+      )
+      .subscribe(_=>{
+        if(_.amount.compact() !== 0){
+          const mosaicId = new symbolSdk.MosaicId(_.id.toHex());
+          this.mosaicRepository.getMosaic(mosaicId).subscribe(m=>this.mosaics.unshift({'mosaicInfo':m, 'mosaic': _}));
+          this.$refs.mosaic_list.update();
+        }
+      });
+    },
+    openListener(){
+      const wsEndpoint = NODE.replace('http', 'ws') + "/ws";
+      this.listener = new symbolSdk.Listener(wsEndpoint, this.namespaceRepository, WebSocket);
+      this.listener.open().then(()=>{
+        this.$refs.tx.show = true;
+        console.log('listener open');
+        this.listener.newBlock().subscribe((block) => {
+          this.$refs.tx.show = true;
+          this.newBlock(block);
+        },
+        (err) => {
+          try{
+            this.$refs.tx.show = false;
+          }catch(e){
+            console.log("listener error");
+            console.log(e);
+          }
+        });
+      });
+
+      this.listener.webSocket.onclose = async function(){
+        try {
+          console.log("listener onclose");
+          this.$refs.tx.show = false;
+        } catch (error) {}
+      }
+    },
+    closeListenr(){
+      this.listener.close();
+      this.listener = null;
+      try {
+        this.$refs.tx.show = false;
+      } catch (error) {}
+      console.log("listener onclose");
+    },
+    copy(){
+      console.log("address copy");
+      navigator.clipboard.writeText(this.g_rawAddress.plain())
+        .then(() => {
+            console.log("copied!");
+            this.snackbarMsg = "address copy";
+            this.color = "success";
+            this.snackbar = true;
+        })
+        .catch(e => {
+            console.error(e)
+        })
+    },
+    newBlock(block){
+      //受信
+      this.transactionRepository.search({
+        address: this.g_rawAddress,
+        height: block.height,
+        group: symbolSdk.TransactionGroup.Confirmed,
+      })
+      .subscribe(_ =>{
+        if(_.data.length > 0){
+          this.parseTx(_.data, true);
+          this.$set(this, "mosaics", []);
+          this.getAccountInfo();
+        }
+      })
+    },
+    parseTx(transaction, newB=null){
+      transaction.forEach((tx) => {
+        //トランスファートランザクションのみ処理
+        if(tx.type === symbolSdk.TransactionType.TRANSFER){
+          if(tx.recipientAddress.plain() === this.g_rawAddress.plain()){
+            tx.mosaics.forEach(mosaic => {
+              if(!this.nglist.find((mos) => mos === mosaic.id.toHex()) && mosaic.amount.compact() !== 0){
+                if(newB){tx.newB = true;}
+                this.histories.receive_mosaics.push(tx);
+                if(!this.init){
+                  this.histories.receive_mosaics.unshift(tx);
+                  this.soundPlay();
+                }
+              }
+            });
+          }else{
+            tx.mosaics.forEach(mosaic => {
+              if(!this.nglist.find((mos) => mos === mosaic.id.toHex()) && mosaic.amount.compact() !== 0){
+                if(newB){tx.newB = true;}
+                this.histories.send_mosaics.push(tx);
+                if(!this.init){
+                this.histories.send_mosaics.unshift(tx);
+                  this.soundPlay();
+                }
+              }
+            });
+          }
+        }
+      });
+    },
+    soundPlay(){
+      this.recipientAudio.currentTime = 0 
+      this.recipientAudio.play();
+      this.snackbarMsg = "transaction receiving";
+      this.color = "orange darken-2";
+      this.snackbar = true;
+    },
+  },
+}
+</script>
---
+++ app/src/views/Meta.vue
@@ -0,0 +1,177 @@
+<template>
+  <div>
+    <v-container>
+       <v-card elevation="2" class="mb-3">
+        <v-card-title>
+          CreateMetaData
+        </v-card-title>
+        <v-card-text>
+          <form>
+              <v-text-field
+              label="mosaic_id(モザイクID)"
+              hide-details="auto"
+              :rules="mosaicRules"
+              required
+              v-model="inputForm.mosaic_id"
+            ></v-text-field>
+            <v-select
+              v-model="inputForm.dist_num"
+              :items="select_items"
+              label="Number of mosaics to be distributed at the time of the lottery(抽選時のモザイク配布枚数)"
+              required
+            ></v-select>
+            <v-dialog
+              ref="open_dialog"
+              v-model="open_modal"
+              :return-value.sync="inputForm.open_date"
+              persistent
+              width="290px"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="inputForm.open_date"
+                  label="Distribution start date(配布開始日)"
+                  prepend-icon="mdi-calendar"
+                  readonly
+                  v-bind="attrs"
+                  v-on="on"
+                 :disabled="checkbox"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="inputForm.open_date"
+                scrollable
+                :max="inputForm.close_date"
+              >
+                <v-spacer></v-spacer>
+                <v-btn
+                  text
+                  color="primary"
+                  @click="open_modal = false"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  text
+                  color="primary"
+                  @click="$refs.open_dialog.save(inputForm.open_date)"
+                >
+                  OK
+                </v-btn>
+              </v-date-picker>
+            </v-dialog>
+            <v-dialog
+              ref="close_dialog"
+              v-model="close_modal"
+              :return-value.sync="inputForm.close_date"
+              persistent
+              width="290px"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="inputForm.close_date"
+                  label="Distribution end date(配布終了日)"
+                  prepend-icon="mdi-calendar"
+                  readonly
+                  v-bind="attrs"
+                  v-on="on"
+                 :disabled="checkbox"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="inputForm.close_date"
+                scrollable
+                :min="inputForm.open_date"
+              >
+                <v-spacer></v-spacer>
+                <v-btn
+                  text
+                  color="primary"
+                  @click="close_modal = false"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  text
+                  color="primary"
+                  @click="$refs.close_dialog.save(inputForm.close_date)"
+                >
+                  OK
+                </v-btn>
+              </v-date-picker>
+            </v-dialog>
+            <v-checkbox
+              v-model="checkbox"
+              @change="checkbox_change"
+              label="配布期限なし"
+            ></v-checkbox>
+          </form>
+          <v-row>
+            <v-col cols="12">
+              <h3 oncopy="return false">
+                {{ output }} 
+              </h3>
+            </v-col>
+            <v-col cols="12">
+              <v-btn
+                :disabled="inputForm.mosaic_id === ''"
+                color="success"
+                elevation="2"
+                @click="copy()"
+              >
+                Parapeter Copy(メタデータ設定値コピー)
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      inputForm:{
+        mosaic_id: '',
+        dist_num: 1,
+        open_date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        close_date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        scope: 1,
+      },
+      select_items: [1,2,3,4,5,6,7,8,9,10],
+      open_modal: false,
+      close_modal: false,
+      checkbox: false,
+      mosaicRules: [
+        v => !!v || 'mosaic_id is required',
+      ],
+    }
+  },
+  computed: {
+    output(){
+      return this.inputForm;
+    }
+  },
+  methods: {
+    checkbox_change(e){
+      if(e){
+        this.inputForm.scope = 0
+      }else{
+        this.inputForm.scope = 1
+      }
+    },
+    copy(){
+      navigator.clipboard.writeText(JSON.stringify(this.inputForm))
+      .then(() => {
+          console.log("copied!")
+      })
+      .catch(e => {
+          console.error(e)
+      })
+    }
+  },
+}
+</script>
